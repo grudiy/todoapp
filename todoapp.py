459bea566df1a7644bc125c016f1b2cdd868a679
Andrii G <gruand@gmail.com>
2024-02-09 21:40:32 +0200

    second

--- todoapp.py
+++ todoapp.py
@@ -1,29 +1,38 @@
 todos = []
 while True:
-    user_action = input("Type: add, edit, show, exit: ")
+    user_action = input("Type: add, edit, show, done, exit: ")
     match user_action:
         case "add":
             user_input = input("Enter a todo: ")
             todos.append(user_input)
             print("Added: ", user_input)
+
         case "edit":
-            todo_number = input("Enter todo number:")
-            todo_number = int(todo_number) - 1
-            print("Editing ", todos[todo_number])
+            number = int(input("Enter todo number for edit:"))
+            number = number - 1
+            print("Editing ", todos[number])
             new_todo = input("New value: ")
-            todos[todo_number] = new_todo
-            print("Saved: ", todos[todo_number])
+            todos[number] = new_todo
+            print("Saved: ", todos[number])
+
         case "show":
-            for item in todos:
-                print(todos.index(item) + 1, ": ",  item)
+            for index, item in enumerate(todos):
+                #f - strings
+                row = f"{index + 1}-{item}"
+                print(row)
+
+        case "done":
+            number = int(input("Enter todo number for complete:"))
+            number = number - 1
+            todos.pop(number)
+            print("Completed.")
+
         case "exit":
             break
         case _:
             print("Invalid choice")
 
 print("bye")
-
-
 
 
 
