4768a1bfd49cd211e9655ec19d3c6ebd2fe16500
Andrii G <gruand@gmail.com>
2024-02-14 22:11:31 +0200

    try except, parse commands with startswith

--- todoapp.py
+++ todoapp.py
@@ -1,8 +1,9 @@
 
 while True:
-    user_action = input("Type: add, edit, show, done, exit: ")
+    user_action = input("Type: add, edit(#), show, done, exit: ")
+    user_action = user_action.strip()
 
-    if "add" in user_action:
+    if user_action.startswith("add"):
         todo = user_action[4:] + '\n'
         with open("todos.txt", "r") as file:
             todos = file.readlines()
@@ -10,41 +11,47 @@ while True:
         with open("todos.txt", "w") as file:
             file.writelines(todos)
 
-    elif "show" in user_action:
+    elif user_action.startswith("show"):
         with open("todos.txt", "r") as file:
             todos = file.readlines()
-        # strip /n with the list comprehensions
-        # new_todos = [item.strip('\n') for item in todos]
         for index, item in enumerate(todos):
             item = item.strip('\n')
             row = f"{index + 1}.{item}"
             print(row)
 
-    elif "edit" in user_action:
-        number = int(user_action[5:])
-        number = number - 1
-        with open("todos.txt", "r") as file:
-            todos = file.readlines()
-        print("Editing ", todos[number])
-        new_todo = input("New value: ") + "\n"
-        todos[number] = new_todo
-        with open("todos.txt", "w") as file:
-            file.writelines(todos)
-        print("Saved: ", todos[number])
+    elif user_action.startswith("edit"):
+        try:
+            number = int(user_action[5:])
+            number = number - 1
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
+            print("Editing ", todos[number])
+            new_todo = input("New value: ") + "\n"
+            todos[number] = new_todo
+            with open("todos.txt", "w") as file:
+                file.writelines(todos)
+            print("Saved: ", todos[number])
+        except ValueError:
+            print("You should enter number")
+            continue
 
-    elif "done" in user_action:
-        number = int(user_action[5:])
-        index = number - 1
-        with open("todos.txt", "r") as file:
-            todos = file.readlines()
-        todo_to_remove = todos[index].strip('\n')
-        todos.pop(index)
-        with open("todos.txt", "w") as file:
-            file.writelines(todos)
-        message = f"Completed: {todo_to_remove}"
-        print(message)
+    elif user_action.startswith("done"):
+        try:
+            number = int(user_action[5:])
+            index = number - 1
+            with open("todos.txt", "r") as file:
+                todos = file.readlines()
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+            with open("todos.txt", "w") as file:
+                file.writelines(todos)
+            message = f"Completed: {todo_to_remove}"
+            print(message)
+        except IndexError:
+            print("There is no such number")
+            continue
 
-    elif "exit" in user_action:
+    elif user_action.startswith("exit"):
         break
 
     else:
