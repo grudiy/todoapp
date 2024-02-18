c2750838011a19b07ed6c39f04c64d748e1b9dc7
gruand <gruand@gmail.com>
2024-02-18 19:33:38 +0200

    Implement Add case

--- todoapp_gui.py
+++ todoapp_gui.py
@@ -1,11 +1,25 @@
 import PySimpleGUI as gui
+import functions
 
 label = gui.Text('Type in a ToDo')
-input_box = gui.InputText(tooltip='Enter ToDo')
+input_box = gui.InputText(tooltip='Enter ToDo', key="todo")
 choose_button = gui.Button('Add')
 
-
 window = gui.Window('My ToDo App',
-                    layout=[[label, input_box, choose_button]])
-window.read()
+                    layout=[[label], [input_box, choose_button]],
+                    font=('Helvetica', 16))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values['todo'] + '\n'
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case gui.WIN_CLOSED:
+            break
+
 window.close()
